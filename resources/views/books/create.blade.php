d9f11ef64270ad6c2aea8c5656286e85bb2587b0
INAM UL HAQ <inamulhaq925@gmail.com>
2023-05-24 11:03:02 +0500

    final commit

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -2,10 +2,10 @@
     <x-setting heading="Add a new Book">
         <form action="/books" method="POST" enctype="multipart/form-data">
             @csrf
-            <x-form.input name="title" />
-            <x-form.input name="author" />
-            <x-form.input name="edition" />
-            <x-form.input name="no_of_pages" type="number" />
+            <x-form.input name="title" required />
+            <x-form.input name="author" required />
+            <x-form.input name="edition" required />
+            <x-form.input name="no_of_pages" type="number" required />
 
             <x-form.button>Add</x-form.button>
         </form>
